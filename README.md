11f7be303a6102cb08ce8459dab7a7c1979e4366
oliver <loliverhennigh101@gmail.com>
2018-01-05 13:59:37 -0600

    readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DeepDesign
-Automated Design Library using Deep Neural Networks
+
+Automated Design Library using Deep Neural Networks. The proposed method can be found [here](https://arxiv.org/abs/1710.10352). The original source code can be found [here](https://github.com/loliverhennigh/Flow-Sculpter) although this repository is quite messy. A first version of this library will be available late February. 
--- deepdesign/data_generator.py
+++ deepdesign/data_generator.py
@@ -8,17 +8,19 @@ from tqdm import *
 from collections import Counter
 
 class DataGenerator:
-  def __init__(self, run_script, num_runs=1000, availible_cpu_cores=4, needs_gpu=False, availible_gpus=[0]):
-    self.pl = []
-    self.running_pl = []
-    self.needs_gpu = needs_gpu
-    self.availible_cpu_cores=availible_cpu_cores
-    self.availible_gpus=availible_gpus
+  def __init__(self, run_script, config):
+    self.num_runs = config.num_runs
+    self.needs_gpu = config.needs_gpu
+    self.availible_cpu_cores = config.availible_cpu_cores
+    self.availible_gpus = config.availible_gpus.split(',')
     self.start_time = 0
 
-  def start_next(self, gpu):
-    for i in xrange(len(self.pl)):
-      if self.pl[i].get_status() == "Not Started":
+    self.sims = []
+
+  def start_sim(self):
+    for i in xrange(len(self.sims)):
+      if self.needs_gpu:
+      if self.sims[i].get_status() == "Not Started":
         self.pl[i].start(gpu)
         break
 
@@ -112,5 +114,61 @@ class DataGenerator:
       self.update_pl_status()
       self.print_que_status()
       
+class Sim:
+  def __init__(self, xml_filename, finish_script):
+    root = etree.parse(xml_filename)
+    command = root.find("cmd").text
+    self.cmd = command
+    self.xml_file = xml_filename
+    self.finish_script = finish_script
+    self.status = "Not Started"
+    self.gpu = -1
+    self.process = None
+    self.return_status = "NONE"
+    self.run_time = 0
+
+  def start(self, gpu=0):
+    with open(os.devnull, 'w') as devnull:
+      self.process = ps.subprocess.Popen(self.cmd.split(' '), stdout=devnull, stderr=devnull, env=dict(os.environ, CUDA_VISIBLE_DEVICES=str(gpu)))
+    self.pid = self.process.pid
+
+    self.status = "Running"
+    self.start_time = time.time()
+    self.gpu = gpu
+
+  def update_status(self):
+    if self.status == "Running":
+      self.run_time = time.time() - self.start_time
+      if self.process.poll() is not None:
+        self.status = "Finished"
+        if self.process.poll() == 0:
+          self.return_status = "SUCCESS"
+          self.finish_script(self.xml_file)
+        else:
+          self.return_status = "FAIL"
+  def get_pid(self):
+    return self.pid
+
+  def get_status(self):
+    return self.status
+
+  def get_gpu(self):
+    return self.gpu
+
+  def get_return_status(self):
+    return self.return_status
+
+  def print_info(self):
+    print_string = (colored('cmd is ', 'blue') + ' '.join(self.cmd) + '\n').ljust(40)
+    print_string = print_string + (colored('status ', 'blue') + self.status + '\n').ljust(30)
+    if self.return_status == "SUCCESS":
+      print_string = print_string + (colored('return status ', 'blue') + colored(self.return_status, 'green') + '\n').ljust(40)
+    elif self.return_status == "FAIL":
+      print_string = print_string + (colored('return status ', 'blue') + colored(self.return_status, 'red') + '\n').ljust(40)
+    else:
+      print_string = print_string + (colored('return status ', 'blue') + colored(self.return_status, 'yellow') + '\n').ljust(40)
+    print_string = print_string + (colored('run time ', 'blue') + str(self.run_time)).ljust(40)
+    print(print_string)
+
 
 
